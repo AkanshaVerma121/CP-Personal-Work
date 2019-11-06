8231f187207f7f496558fe049e874ecce4642362
AkanshaVerma121 <46772456+AkanshaVerma121@users.noreply.github.com>
2019-11-06 13:00:24 +0530

    Changing code of paid desktpop

--- src/com/thinksys/Desktop/PaidDesktop.java
+++ src/com/thinksys/Desktop/PaidDesktop.java
@@ -1,8 +1,10 @@
 package com.thinksys.Desktop;
 
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,74 +16,49 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class PaidDesktop {
 	
 	public static void main(String[] args) {
-
-		String ExpectedZiperror = "Please enter a valid zip/postal code.";
-		String ExpectedGGSerror = "Please make a relationship selection.";
+		
+	
 
 		System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");  
 		WebDriver driver= new ChromeDriver();
 		driver.get("https://www.match.com/cpx/en-us/landing/display/84123/");
 
-		WebElement Zip = driver.findElement(By.id("zipField"));
 		WebElement Clickbutton = driver.findElement(By.className("btn-progressive"));
-
-		///////////////////////  MOdal 1: ZIP /////////////////////////////////////////////
-
-		//Case 1: When Zip is empty
-
+		WebElement Zip = driver.findElement(By.id("zipField"));
+        Zip.sendKeys("75225");
 		Clickbutton.click();
-		String ZipError = driver.findElement(By.className("error")).getText();
-		               if (ZipError.contentEquals(ExpectedZiperror))
-		                { System.out.println("Error message on empty Zip is correct. It is" +ZipError);}
-		               else
-		                {System.out.println("Error message on Zip is not correct");}
+		
+		WebDriverWait wait = new WebDriverWait(driver, 120);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("genderGenderSeek")));
 
-		//Case 2: When Zip is invalid
-
-		Zip.sendKeys("ffbfdgdgf");
-		Clickbutton.click();
-		String ZipError2 = driver.findElement(By.className("error")).getText();
-		if (ZipError2.contentEquals(ExpectedZiperror))
-		{ System.out.println("Error message on invalid Zip is correct. It is " +ZipError2);}
-		else
-		{System.out.println("Error message on invalid Zip is not correct");}
-
-		//Case 3: When Zip is valid
-
-		Zip.clear();
-		Zip.sendKeys("75225");
-		Clickbutton.click();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-		///////////////// Modal 2: GGS ///////////////////////
-	    List<WebElement> e1 = driver.findElements(By.id("genderGenderSeek"));
-	    System.out.println(e1);
+		 Select GGSdrpdn = new Select (driver.findElement(By.name("genderGenderSeek")));
+		GGSdrpdn.selectByIndex(2);
+		
+		//driver.findElement(By.className("btn-view-photos btn-progressive")).click();
+		List<WebElement> elements = driver.findElements(By.className("button"));
+		elements.get(1).click();
+		
+		//List<WebElement> element2 = driver.findElements(By.id("emailField"));
+		//System.out.println("Number of elements" +element2.size()) ;
+		
+		
+		Random randomGenerator = new Random();  
+		int randomInt = randomGenerator.nextInt(1000); 
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("emailField")));
+		driver.findElement(By.id("emailField")).sendKeys("testakku"+ randomInt +"@gmail.com");
+		elements.get(1).click();
 	
 		
-		Select GGSdrp = new Select(driver.findElement(By.id("genderGenderSeek")));
-		GGSdrp.selectByIndex(2);
-		/////////Modal 3: Email Modal //////////////////////////
-
-	/*
-	driver.findElement(By.id("emailField")).sendKeys("hjgashujh@dgmaild.com");
-
-		WebElement Emailbutton = driver.findElement(By.xpath("//img[@src = 'https://securecp.match.com//cppp/floatingreg/images/ctnbtn.png']"));
-		Emailbutton.click();
-
 		//// Modal 4: Password Field ////////
 
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("passwordField")));
 		driver.findElement(By.id("passwordField")).sendKeys("test");
-
-		WebElement Passwordbutton = driver.findElement(By.xpath("//img[@src = 'https://securecp.match.com//cppp/floatingreg/images/ctnbtn.png']"));
-		Passwordbutton.click();
+		elements.get(1).click();
+	
 
 		/////////// Modal 5: DOB Modal ////////////////////
 
-
-		List<WebElement> ele =  driver.findElements(By.name("birthMonth"));
-		System.out.println(ele.size());
-
-		WebDriverWait wait = new WebDriverWait(driver, 120);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("birthMonth")));
 
 		Select Monthdrp = new Select(driver.findElement(By.name("birthMonth")));
@@ -93,24 +70,17 @@ public class PaidDesktop {
 
 		Select Yeardrp = new Select(driver.findElement(By.name("birthYear")));
 		Yeardrp.selectByIndex(25);
-
-
-		WebElement Dobbutton = driver.findElement(By.xpath("//img[@src = 'https://securecp.match.com//cppp/floatingreg/images/ctnbtn.png']"));
-		Dobbutton.click();
+		elements.get(1).click();
 
 
 		////// lqst modal ///
 
-		driver.findElement(By.id("handleField")).sendKeys("hggj");
-		driver.findElement(By.xpath("//img[@src = 'https://securecp.match.com//cppp/floatingreg/images/ctnbtn.png']")).click();;
-
-
-		//List<WebElement> elements = driver.findElements(By.xpath("//img[@src = 'https://securecp.match.com//cppp/floatingreg/images/ctnbtn.png']"));
-		//System.out.println("Number of elements" +elements.size()) ;
-		//System.out.println("Number of elements:" +listOfElements.size());
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("handleField")));
+		String Randmname = RandomStringUtils.randomAlphabetic(4);
+		driver.findElement(By.id("handleField")).sendKeys("test"+ Randmname +"av");
+		elements.get(1).click();
 		
-		
-		*/
+	
 		}
 
 		}
